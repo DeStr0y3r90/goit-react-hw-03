ccd60d5754e84998719eeb00a6d14691009f26f2
DeStr0y3r90 <98543747+DeStr0y3r90@users.noreply.github.com>
2024-06-25 18:45:35 +0100

    + user

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ export function App() {
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    { id: "id-4", name: "Vasyl Kozhokar", number: "555-55-55" },
   ];
   const [contacts, setContacts] = useState(() => {
     const savedContact = localStorage.getItem("my-key");
