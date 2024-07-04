d383f09ef59a164e9e3415a4ea8ec0da4b09a03a
DeStr0y3r90 <98543747+DeStr0y3r90@users.noreply.github.com>
2024-07-04 18:48:55 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { ContactList } from "./сomponents/ContactList/ContactList.jsx";
+import { ContactList } from "./components/ContactList/ContactList.jsx";
 import { SearchBox } from "./сomponents/SearchBox/SearchBox.jsx";
 import { ContactForm } from "./сomponents/ContactForm/ContactForm.jsx";
 
@@ -11,7 +11,6 @@ export function App() {
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    { id: "id-4", name: "Vasyl Kozhokar", number: "555-55-55" },
   ];
   const [contacts, setContacts] = useState(() => {
     const savedContact = localStorage.getItem("my-key");
